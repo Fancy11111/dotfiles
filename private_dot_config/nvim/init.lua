953e3f0f129fc8045082d6d6420b86330706c000
Daniel Fenz <daniel@fenz.io>
2023-02-19 12:11:51 +0100

    changed nvim chonfig to lazy

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,1 @@
--- require("plugin.packer_compiled")
 require("daniel")
--- private_dot_config/nvim/lua/daniel/lazy.lua
+++ private_dot_config/nvim/lua/daniel/lazy.lua
@@ -4,45 +4,147 @@ if not vim.loop.fs_stat(lazypath) then
         "git",
         "clone",
         "--filter=blob:none",
-        "--single-branch",
         "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
         lazypath,
     })
 end
-vim.opt.runtimepath:prepend(lazypath)
-local opts = {
-    defaults = { lazy = true },
-    dev = { patterns = jit.os:find("Windows") and {} or { "folke" } },
-    install = { colorscheme = { "tokyonight", "habamax" } },
-    checker = { enabled = true },
-    diff = {
-        cmd = "terminal_git",
+vim.opt.rtp:prepend(lazypath)
+
+
+local plugins = {
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'nvim-lua/plenary.nvim',
+    { 'nvim-treesitter/nvim-treesitter',         build = ':TSUpdate' },
+    { 'nvim-treesitter/nvim-treesitter-context', dependencies = { 'nvim-treesitter/nvim-treesitter' } },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        dependencies = { { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' } }
     },
-    performance = {
-        cache = {
-            enabled = true,
-            -- disable_events = {},
-        },
-        rtp = {
-            disabled_plugins = {
-                -- "gzip",
-                -- "matchit",
-                -- "matchparen",
-                -- "netrwPlugin",
-                -- "tarPlugin",
-                -- "tohtml",
-                -- "tutor",
-                -- "zipPlugin",
-                -- "nvim-treesitter-textobjects",
-            },
-        },
+    'nvim-telescope/telescope-file-browser.nvim',
+    'nvim-telescope/telescope-media-files.nvim',
+    'ThePrimeagen/git-worktree.nvim',
+    {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
     },
-    ui = {
-        custom_keys = {
-            -- ["<localleader>d"] = function(plugin)
-            --     dd(plugin)
-            -- end,
-        },
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lua',
+    {
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup({
+                disable_filetype = { 'TelescopePrompt', 'vim' }
+            })
+        end
     },
-}
-require("lazy").setup("plugins", opts)
+    'mfussenegger/nvim-jdtls',
+    { 'folke/neodev.nvim' },
+
+    'nvim-tree/nvim-web-devicons',
+    {
+        "glepnir/lspsaga.nvim",
+        event = "BufRead",
+        config = function()
+            require("lspsaga").setup({})
+        end,
+        dependencies = { { "nvim-tree/nvim-web-devicons" } }
+    },
+    -- Debugging
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    {
+        "leoluz/nvim-dap-go",
+        dependencies = { { "nvim-dap" } }
+    },
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            { 'hrsh7th/cmp-cmdline' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    },
+
+    {
+        "ray-x/go.nvim",
+        requires = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", 'gomod' },
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
+
+    { 'ahmedkhalf/project.nvim' },
+    { 'goolord/alpha-nvim',     dependencies = { 'ahmedkhalf/project.nvim' } },
+    {
+        'NvChad/nvim-colorizer.lua',
+        name = "colorizer",
+        config = function()
+            require("colorizer").setup({
+                filetypes = { '*' },
+                css = {
+                    rgb_fn = true
+                },
+                RRGGBBAA = true
+            })
+        end
+    },
+
+    -- Appearance
+    'Yggdroot/indentLine',
+    { 'nvim-lualine/lualine.nvim' },
+    { 'akinsho/bufferline.nvim',  tag = "v3.2.0", dependencies = { 'nvim-tree/nvim-web-devicons' } },
+    { 'ThePrimeagen/harpoon' },
+
+    -- Colorschemes
+    "folke/tokyonight.nvim",
+    'andersevenrud/nordic.nvim',
+    'EdenEast/nightfox.nvim',
+    'gruvbox-community/gruvbox',
+
+    { "catppuccin/nvim", name = "catppuccin" },
+
+    {
+        'linty-org/key-menu.nvim',
+        config = function()
+            require 'key-menu'.set('n', '<Space>')
+        end
+    } }
+
+require('lazy').setup(plugins)
