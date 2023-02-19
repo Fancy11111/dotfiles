b6fbdd3a3d14216c2f1999f2a814af92987f20e4
Daniel Fenz <daniel@fenz.io>
2023-02-19 13:34:49 +0100

    changed nvim chonfig to lazy

--- private_dot_config/nvim/lua/daniel/lsp.lua
+++ private_dot_config/nvim/lua/daniel/lsp.lua
@@ -28,7 +28,7 @@ local lsp = require('lsp-zero').preset({
         name = 'minimal',
         set_lsp_keymaps = true,
         manage_nvim_cmp = true,
-        suggest_lsp_servers = false,
+        suggest_lsp_servers = true,
     })
 
 lsp.configure('lua_lsp', {
