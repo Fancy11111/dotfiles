cea5eac1d8600b7fb809f2fb25c814dfe28213d3
Daniel Fenz <daniel@fenz.io>
2025-06-02 23:55:42 +0200

    added wip nvim config

---
+++ private_dot_config/nvim-new/lua/daniel/init.lua
@@ -0,0 +1,11 @@
+require("daniel.opts")
+
+require("daniel.lazy")
+
+require("daniel.autocmds")
+require("daniel.keymaps")
+
+vim.cmd.colorscheme("tokyonight-night")
+
+-- You can configure highlights by doing something like:
+vim.cmd.hi("Comment gui=none")
