a46dd2b27faa73f1993ae8a55ac13d60a24b41e5
Daniel Fenz <daniel@fenz.io>
2025-12-23 16:29:21 +0100

    updated dotfiles

--- private_dot_config/nvim/lua/daniel/plugins/typst-preview.lua
+++ private_dot_config/nvim/lua/daniel/plugins/typst-preview.lua
@@ -2,5 +2,12 @@ return {
 	"chomosuke/typst-preview.nvim",
 	lazy = false, -- or ft = 'typst'
 	version = "1.*",
-	opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+	opts = {
+		port = 12222,
+		invert_colors = "auto",
+		follow_cursor = false,
+	}, -- lazy.nvim will implicitly calls `setup {}`,
+	select = function(opts)
+		require("typst-preview").setup(opts)
+	end,
 }
